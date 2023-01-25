5f13d35ade1b7b62adab0aba7e8938f9ee9f0a41
Daniel Mafinski Biz <danielmbiz2@gmail.com>
2023-01-25 15:37:22 -0300

    Incluindo POST no /users

--- src/main/java/com/example/authjwt/controllers/UserController.java
+++ src/main/java/com/example/authjwt/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.example.authjwt.controllers;
 
+import com.example.authjwt.entities.User;
 import com.example.authjwt.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,8 +18,13 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
     @GetMapping
-    public ResponseEntity<?> getAllUsers() {
+    public ResponseEntity<?> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
 }
--- src/main/java/com/example/authjwt/services/UserService.java
+++ src/main/java/com/example/authjwt/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.authjwt.services;
 import com.example.authjwt.entities.User;
 import com.example.authjwt.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,13 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return repository.save(user);
+    }
     public List<User> findAll() {
         return repository.findAll();
     }
